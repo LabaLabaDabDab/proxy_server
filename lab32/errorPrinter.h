40d69c27783aa7cd065db46c19d8956023b11e92
LabaLabaDabDab <61375062+LabaLabaDabDab@users.noreply.github.com>
2023-01-23 17:21:48 +0700

    Delete lab32 directory

--- lab32/errorPrinter.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void print_error(const char *prefix, int code);
-void print_error2(const char *prefix1, const char *prefix2, int code);
--- lab32/script1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for i in {1..510};
-do wget http://www.ccfit.nsu.ru/~rzheutskiy/test_files/500mb.dat -e use_proxy=on -e http_proxy=127.0.0.1:20000 -O /dev/null && date &
-done
