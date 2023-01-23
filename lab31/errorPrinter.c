336aa3b6b00474e4fb073565d579c85331b83b58
LabaLabaDabDab <61375062+LabaLabaDabDab@users.noreply.github.com>
2023-01-23 17:16:18 +0700

    Delete lab31 directory

--- lab31/errorPrinter.c
+++
@@ -1,9 +0,0 @@
-#include "errorPrinter.h"
-
-void print_error(const char *prefix, int code) {
-    char buf[256];
-    if (0 != strerror_r(code, buf, sizeof(buf))) {
-        strcpy(buf, "(unable to generate error!)");
-    }
-    fprintf(stderr, "%s: %s\n", prefix, buf);
-}
--- lab31/script1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for i in {1..510};
-do wget http://84.237.52.20/~rzheutskiy/test_files/500mb.dat -e use_proxy=on -e http_proxy=127.0.0.1:20001 -O /dev/null && date &
-done
